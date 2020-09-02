097a2c5e0ef67c4860f32375e29441b1cdabda45
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-02 06:21:02 -0500

    Added blog images dummy content and grid layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,6 +7,15 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box'
 import Image from '../src/img/F.png';
 import Logo from '../src/img/logo.png';
+import { Container } from '@material-ui/core/';
+import { Grid } from '@material-ui/core/';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+
 
 
 
@@ -17,11 +25,11 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#fff"
   },
   logo:{
-    width: '75px',
-    height: '75px',
+    width: '150px',
+    height: '150px',
     borderRadius: '50%',
     overflow: 'hidden',
-    // margin-top: -6px;
+    position: "center",
   },
   hero: {
     backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0, 0, 0, 0.5)), url(${Image})`,
@@ -35,6 +43,22 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     color: "#fff",
     fontSize: "4rem",
+  },
+  blogsContainer:{
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle:{
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3),
+    float: "left",
+    marginLeft: "0",
+  },
+  card: {
+    maxWidth: '75%'
+  },
+  media: {
+    height: 540,
+    maxWidth: '75%'
   }
 }));
 
@@ -46,9 +70,6 @@ function App() {
     <div className="App">
     <AppBar className={classes.appBar} position="static">
         <Toolbar>
-          {/* <Typography variant="h6" color="primary">
-            Blog
-          </Typography> */}
           <img className={classes.logo} src={Logo} alt="Logo" />
         </Toolbar>
 
@@ -57,6 +78,191 @@ function App() {
       <Box>
         <Box className={classes.hero}> React Blog w/ Hero Image</Box>
       </Box>
+
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+          <Typography variant="h4" className={classes.blogTitle}>
+            Articles
+          </Typography>
+      </Container>
+
+      <Grid container spacing={(3)}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/chicken.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/cow.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/eggplant.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/farm.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/goat.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+
+      <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={require('../src/img/squash.png')}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+          </Card>
+        </Grid>
+        </Grid>
+
+
+
     </div>
   );
 }
