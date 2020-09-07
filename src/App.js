ebf049a54d4c81b8090f9677ea558f239ed21736
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-06 19:45:12 -0500

    Rebuilding app.js and reorganizing components to run logic

--- src/App.js
+++ src/App.js
@@ -1,38 +1,133 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
 import MyApp from './MyApp';
-import Navbar from '../src/components/Navbar';
-import Footer from '../src/components/Footer';
+import Grid from '@material-ui/core/Grid';
 import CurrentArticles from '../src/components/CurrentArticles';
-import PopularArticles from '../src/components/PopularArticles';
-import Header from '../src/components/Header';
-import NewsLetterForm from '../src/components/NewsletterForm';
-import SideBar from '../src/components/SideBar';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import {Switch, Route, Redirect } from 'react-router-dom';
-import Home from './components';
+import Main from './components/Main';
+import Navbar from './components/Navbar.js';
+import Sidebar from './components/SideBar';
+import Footer from './components/Footer';
 
 
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+  const currentArticles = [
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: '../img/chicken.png',
+      imageText: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: '../img/cow.png',
+      imageText: 'Image Text',
+    },
+    {
+      title: 'Featured post',
+      date: 'Nov 12',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: '../img/eggplant.png',
+      imageText: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: '../img/farm.png',
+      imageText: 'Image Text',
+    },
+    {
+      title: 'Post title',
+      date: 'Nov 11',
+      description:
+        'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: '../img/goat.png',
+      imageText: 'Image Text',
+    },
+  ];
+
+  const posts = [];
+
+  const sidebar = {
+    title: 'About',
+    description:
+      'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+    archives: [
+      { title: 'March 2020', url: '#' },
+      { title: 'February 2020', url: '#' },
+      { title: 'January 2020', url: '#' },
+      { title: 'November 1999', url: '#' },
+      { title: 'October 1999', url: '#' },
+      { title: 'September 1999', url: '#' },
+      { title: 'August 1999', url: '#' },
+      { title: 'July 1999', url: '#' },
+      { title: 'June 1999', url: '#' },
+      { title: 'May 1999', url: '#' },
+      { title: 'April 1999', url: '#' },
+    ],
+    social: [
+      { name: 'GitHub', icon: GitHubIcon },
+      { name: 'Twitter', icon: TwitterIcon },
+      { name: 'Facebook', icon: FacebookIcon },
+    ],
+  };
 
 function App() {
-
-//   const [posts, setPosts] = useState(['article1', 'article2', 'article3', 'article4', 'article5', 'article6', 'article7', 'article8', 'article9', 'article10']);
-// console.log(" YOU HAVE THIS MANY ARTICLES :" + posts.length);
+  const classes = useStyles();
   return (
     <ScopedCssBaseline>
     <div className="App">
-        <Home />
-        {/* <Header /> */}
-        <CurrentArticles />
-        {/* <SideBar /> */}
+    <Container maxWidth="lg">
+        <Navbar title="Blog" sections={sections} />
+        <main>
 
-    <Switch>
-        <Route exact path="/" render={props => <PopularArticles{...props} />} />
-    </Switch>
-
-
-        <NewsLetterForm />
-        <Footer />
-
+          <Grid container spacing={4}>
+            {currentArticles.map((post) => (
+              <CurrentArticles key={post.title} post={post} />
+            ))}
+          </Grid>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title="Title of something." posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+      <Footer title="Footer" description="Something here to give the footer a purpose!" />
         <MyApp/>
     </div>
     </ScopedCssBaseline>
--- src/components/CurrentArticles.js
+++ src/components/CurrentArticles.js
@@ -12,6 +12,8 @@ import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Hidden from '@material-ui/core/Hidden';
+import Index from './index';
+
 
 
 
