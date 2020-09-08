8e96908377421532398c558b37874ee092fd8a0c
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-08 13:05:13 -0500

    Updates

---
+++ precache-manifest.550fcb731c2aace23725c35386f713b2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48771b956b690c219758261c22a991c",
+    "url": "/React-Blog-Layout/index.html"
+  },
+  {
+    "revision": "5fe14fcb4e1b9a2d72c1",
+    "url": "/React-Blog-Layout/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "5fe14fcb4e1b9a2d72c1",
+    "url": "/React-Blog-Layout/static/js/2.68601fe6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Blog-Layout/static/js/2.68601fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5088d621d81d51f08826",
+    "url": "/React-Blog-Layout/static/js/main.513c2be4.chunk.js"
+  },
+  {
+    "revision": "e8e8465a76f5e5c651b5",
+    "url": "/React-Blog-Layout/static/js/runtime-main.388044ac.js"
+  },
+  {
+    "revision": "35a79d560f078d1b631e2912ef6f4e65",
+    "url": "/React-Blog-Layout/static/media/HeroImage.35a79d56.png"
+  },
+  {
+    "revision": "4244940eff779ebc3a426948dc642749",
+    "url": "/React-Blog-Layout/static/media/chicken.4244940e.png"
+  },
+  {
+    "revision": "3fd8109954bda654b6a36a1074ece1b9",
+    "url": "/React-Blog-Layout/static/media/cow.3fd81099.png"
+  },
+  {
+    "revision": "1c81f7630b7b57180cf1103742f39e75",
+    "url": "/React-Blog-Layout/static/media/eggplant.1c81f763.png"
+  },
+  {
+    "revision": "4dbec4b6ae5e58f7a842a65f7dd1f9f7",
+    "url": "/React-Blog-Layout/static/media/farm.4dbec4b6.png"
+  },
+  {
+    "revision": "0cb770b8e261e8f5a47e1c9f8952278c",
+    "url": "/React-Blog-Layout/static/media/goat.0cb770b8.png"
+  },
+  {
+    "revision": "915e6d3fd74fe6431b89d9fd7c524fe5",
+    "url": "/React-Blog-Layout/static/media/logo.915e6d3f.png"
+  }
+]);
