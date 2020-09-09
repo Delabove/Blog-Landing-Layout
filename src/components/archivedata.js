211fa4bbd5f1a8cd9d6eb9b84ccb82e3a23fbcc9
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-09 08:30:42 -0500

    Organized data

---
+++ src/components/archivedata.js
@@ -0,0 +1,176 @@
+import chickens from '../img/chicken.png';
+import cows from '../img/cow.png';
+import eggplant from '../img/eggplant.png';
+import farm from '../img/farm.png';
+import goat from '../img/goat.png';
+
+
+
+
+const archiveData = [
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Life',
+        title: 'Why We Traded The City for the Farm',
+
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Life',
+        title: 'Our Favorite Days on the Farm Look Like This',
+    },
+    {category: 'Favorite Reads',
+    subcategory: 'Farm Basics',
+    title: 'Grain Bin Safety',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Basics',
+        title: 'Soil pH',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Livestock',
+        title: 'Which Livestock is Most Profitable',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Livestock',
+        title: 'The Most Common Livestock For Your Farm',
+
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Seasonal Crops',
+        title: 'Dog Days of Summer',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Seasonal Crops',
+        title: 'Easiest Fall Crops',
+    },
+    {category: 'Favorite Reads',
+    subcategory: 'Farm Equipment',
+    title: 'Essential Equipment for Small Farms',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Equipment',
+        title: 'Rotary Tillers: Why They Are Necessary',
+    },
+
+
+
+
+
+
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Life',
+        title: 'Why We Traded The City for the Farm',
+
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Life',
+        title: 'Our Favorite Days on the Farm Look Like This',
+    },
+    {category: 'Most Talked About',
+    subcategory: 'Farm Basics',
+    title: 'Grain Bin Safety',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Basics',
+        title: 'Soil pH',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Livestock',
+        title: 'Which Livestock is Most Profitable',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Livestock',
+        title: 'The Most Common Livestock For Your Farm',
+
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Seasonal Crops',
+        title: 'Dog Days of Summer',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Seasonal Crops',
+        title: 'Easiest Fall Crops',
+    },
+    {category: 'Most Talked About',
+    subcategory: 'Farm Equipment',
+    title: 'Essential Equipment for Small Farms',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Equipment',
+        title: 'Rotary Tillers: Why They Are Necessary',
+    },
+
+
+
+
+
+
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {category: 'Most Shared',
+    subcategory: 'Farm Life',
+    title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'Farm Fresh Eggs: The Perfect Comeback',
+    },
+  ];
+
+  export default archiveData;
