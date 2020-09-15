5d4e0761ed066118f4556e170b23af2885a50f04
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-15 16:09:29 -0500

    renamed file and reformatting tab code

--- src/components/PopularArticles.js
+++ src/components/PopularArticles.js
@@ -6,195 +6,33 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import categories from '../components/popularPostData';
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-
-      },
-      blogTitle:{
-          marginBottom: theme.spacing(5)
-      }
-
-    }));
+const useStyles = makeStyles({
+  root: {
+    height: 110,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+});
 
 
 function PopularArticles() {
 const classes = useStyles();
     return(
-
- <div className={classes.root}>
-    <Typography variant="h2" className={classes.blogTitle} spacing={5}
-    display="flex"
-    justifyContent="start">
-           Reader's Choice
-    </Typography>
-    <Tabs forceRenderTabPanel defaultIndex={1}>
+<Tabs>
     <TabList>
-      <Tab>Favorite Reads</Tab>
-      <Tab>Most Talked About</Tab>
-      <Tab>Most Shared</Tab>
+      <Tab>Title 1</Tab>
+      <Tab>Title 2</Tab>
     </TabList>
+
     <TabPanel>
-      <Tabs forceRenderTabPanel>
-        <TabList>
-          <Tab>Farm Life</Tab>
-          <Tab>Farm Basics</Tab>
-          <Tab>Livestock</Tab>
-          <Tab>Seasonal Crops</Tab>
-          <Tab>Farm Equipment</Tab>
-        </TabList>
-        <TabPanel>
-        <Box>
-        <Link>Why We Traded The City for the Farm</Link>
-        </Box>
-        <Box>
-        <Link>Our Favorite Days on the Farm Look Like This</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Grain Bin Safety</Link>
-        </Box>
-        <Box>
-        <Link>Soil pH</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Which Livestock is Most Profitable</Link>
-        </Box>
-        <Box>
-        <Link>The Most Common Livestock For Your Farm</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Why We Traded The City for the Farm</Link>
-        </Box>
-        <Box>
-        <Link>Our Favorite Days on the Farm Look Like This</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Essential Equipment for Small Farms</Link>
-        </Box>
-        <Box>
-        <Link>Rotary Tillers: Why They Are Necessary</Link>
-        </Box>
-        </TabPanel>
-      </Tabs>
+      <h2>Any content 1</h2>
     </TabPanel>
     <TabPanel>
-      <Tabs forceRenderTabPanel>
-        <TabList>
-          <Tab>Farm Life</Tab>
-          <Tab>Farm Basics</Tab>
-          <Tab>Livestock</Tab>
-          <Tab>Seasonal Crops</Tab>
-          <Tab>Farm Equipment</Tab>
-        </TabList>
-        <TabPanel>
-        <Box>
-        <Link>A Day On The Farm</Link>
-        </Box>
-        <Box>
-        <Link>My Favorite Season On The Farm</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>How much water do farmers manage</Link>
-        </Box>
-        <Box>
-        <Link>Nutrient Availability</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Why We Traded The City for the Farm</Link>
-        </Box>
-        <Box>
-        <Link>Our Favorite Days on the Farm Look Like This</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>The Easiest Livestock To Begin Farming</Link>
-        </Box>
-        <Box>
-        <Link>The Healthiest Livestock To Raise For The Fridge</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Must Have Blades To Have On Hand At All Times</Link>
-        </Box>
-        <Box>
-        <Link>Lawn Care Equipment</Link>
-        </Box>
-        </TabPanel>
-      </Tabs>
-    </TabPanel>
-    <TabPanel>
-      <Tabs forceRenderTabPanel>
-        <TabList>
-          <Tab>Farm Life</Tab>
-          <Tab>Farm Basics</Tab>
-          <Tab>Livestock</Tab>
-          <Tab>Seasonal Crops</Tab>
-          <Tab>Farm Equipment</Tab>
-        </TabList>
-        <TabPanel>
-        <Box>
-        <Link>How We Started Farming</Link>
-        </Box>
-        <Box>
-        <Link>The Main Reason I Love Farm Life</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Grass Waterways</Link>
-        </Box>
-        <Box>
-        <Link>How Farmers Pick Their Planting Population</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Livestock: For Use or For Pleasure?</Link>
-        </Box>
-        <Box>
-        <Link>Our Favorite Days on the Farm Look Like This</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>Dairy Cattle: The Good and The Bad</Link>
-        </Box>
-        <Box>
-        <Link>The Processing of Home Grown Meat</Link>
-        </Box>
-        </TabPanel>
-        <TabPanel>
-        <Box>
-        <Link>The Tractor I Can't Live Without</Link>
-        </Box>
-        <Box>
-        <Link>My Favorite Old Pickup Truck</Link>
-        </Box>
-        </TabPanel>
-      </Tabs>
+      <h2>Any content 2</h2>
     </TabPanel>
   </Tabs>
-
-</div>
 );
     }
 
---
+++ src/components/popularPostData.js
@@ -0,0 +1,314 @@
+import React from 'react';
+
+
+const categories = [
+    {
+       name : 'Favorite Reads',
+       id: 'read-id',
+        subcategories: [
+        {
+            subject: 'Farm Life',
+            id: 'farmLife-id',
+            post: [{ title: 'Why We Traded The City for the Farm'}],
+        },
+    ],
+
+    },
+
+    {
+        name:  'Favorite Reads',
+        id: 'read-id',
+         subcategories: [
+        {
+            subject: 'Farm Life',
+            id: 'farmLife-id',
+            post: [{ title: 'Our Favorite Days on the Farm Look Like This'}],
+    },
+],
+
+},
+
+{
+    name:  'Favorite Reads',
+    id: 'read-id',
+     subcategories: [
+    {
+        subject: 'Farming Basics',
+        id: 'farmingBasics-id',
+        post: [{ title: 'Grain Bin Safety'}],
+    },
+],
+
+},
+
+{
+    name:  'Favorite Reads',
+    id: 'read-id',
+     subcategories: [
+    {
+        subject: 'Farming Basics',
+        id: 'farmingBasics-id',
+        post: [{  title: 'Soil pH'}],
+    },
+],
+
+    },
+
+{
+    name:  'Favorite Reads',
+    id: 'read-id',
+    subcategories: [
+    {
+        subject: 'Livestock',
+        id: 'livestock-id',
+        post: [{title: 'Which Livestock is Most Profitable'}],
+    },
+],
+
+},
+
+{
+    name:  'Favorite Reads',
+    id: 'read-id',
+    subcategories: [
+    {
+        subject: 'Livestock',
+        id: 'livestock-id',
+        post: [{title: 'The Most Common Livestock For Your Farm'}],
+    },
+],
+
+},
+{
+    name:  'Favorite Reads',
+    id: 'read-id',
+        subcategories: [
+        {
+            subject: 'Seasonal Crops',
+            id: 'seasonalCrops-id',
+            post: [{ title: 'Dog Days of Summer'}],
+    },
+],
+
+        },
+        {
+            name:  'Favorite Reads',
+            id: 'read-id',
+                 subcategories: [
+                {
+                    subject: 'Seasonal Crops',
+                    id: 'sesonalCrops-id',
+                    post: [{ title: 'Easiest Fall Crops'}],
+                },
+            ],
+
+            },
+            {
+                name:  'Favorite Reads',
+                id: 'read-id',
+                     subcategories: [
+                    {
+                        subject: 'Farm Equipment',
+                        id: 'farmEquipment-id',
+                        post: [{ title: 'Essential Equipment for Small Farms'}],
+                    },
+                ],
+
+                },
+                {
+                    name:  'Favorite Reads',
+                    id: 'read-id',
+                         subcategories: [
+                        {
+                            subject: 'Farm Equipment',
+                            id: 'farmEquipment-id',
+                            post: [{ title: 'Rotary Tillers: Why They Are Necessary'}],
+                        },
+                    ],
+
+                    },
+
+
+
+
+                    {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Farm Life',
+                                id: 'farmLife-id',
+                                post: [{ title: 'A Day On The Farm'}],
+                            },
+                        ],
+
+                        },
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Farm Life',
+                                id: 'farmLife-id',
+                                post: [{ title: 'My Favorite Season On The Farm'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Farm Basics',
+                                id: 'farmBasics-id',
+                                post: [{ title: 'How Much Water Do Farmers Manage'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Farm Basics',
+                                id: 'farmBasics-id',
+                                post: [{ title: 'Nutrient Availability'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Livestock',
+                                id: 'livestock-id',
+                                post: [{ title: 'Which Livestock is Most Profitable'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Livestock',
+                                id: 'livestock-id',
+                                post: [{ title: 'The Healthiest Livestock To Raise For The Fridge'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Seasonal Crops',
+                                id: 'seasonalCrops-id',
+                                post: [{ title: 'Interested In Growing Beets?'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                        name:  'Most Talked About',
+                        id: 'comments-id',
+                             subcategories: [
+                            {
+                                subject: 'Seasonal Crops',
+                                id: 'seasonalCrops-id',
+                                post: [{ title: 'Beans: A Warm Weather Favorite'}],
+                            },
+                        ],
+
+                        },
+
+                        {
+                            name:  'Most Talked About',
+                            id: 'comments-id',
+                                 subcategories: [
+                                {
+                                    subject: 'Farm Equipment',
+                                    id: 'farmEquipment-id',
+                                    post: [{ title: 'Must Have Blades To Have On Hand At All Times'}],
+                                },
+                            ],
+
+                            },
+                            {
+                                name:  'Most Talked About',
+                                id: 'comments-id',
+                                     subcategories: [
+                                    {
+                                        subject: 'Farm Equipment',
+                                        id: 'farmEquipment-id',
+                                        post: [{ title: 'Lawn Care Equipment'}],
+                                    },
+                                ],
+
+                                }
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Farm Life',
+    //     name: 'How We Started Farming',
+
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Farm Life',
+    //     subject: 'The Main Reason I Love Farm Life',
+    // },
+    // {category: 'Most Shared',
+    // subcategory: 'Farm Basics',
+    // title: 'Grass Waterways',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Farm Basics',
+    //     title: 'How Farmers Pick Their Planting Population',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Livestock',
+    //     title: 'The Processing of Home Grown Meat',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Livestock',
+    //     subject: 'Dairy Cattle: The Good and The Bad',
+
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Seasonal Crops',
+    //     title: 'How the Heat Affects Crops',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Seasonal Crops',
+    //     title: 'Preparing For Winter',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Farm Equipment',
+    //     title: 'Our Most Used Tractor',
+    // },
+    // {
+    //     category: 'Most Shared',
+    //     subcategory: 'Farm Equipment',
+    //     title: 'My Favorite Old Pickup Truck',
+    // },
+  ];
+
+  export default categories;
